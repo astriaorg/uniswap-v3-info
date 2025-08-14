d67045d6526bb3f9755f60f0808a266f6ade4d19
Jordan Oroshiba <jordan@astria.org>
2025-08-14 13:32:34 -0700

    comma too

--- src/data/application/index.ts
+++ src/data/application/index.ts
@@ -4,7 +4,7 @@ import gql from 'graphql-tag'
 
 export const SUBGRAPH_HEALTH = gql`
   query health($name: Bytes) {
-    indexingStatusForCurrentVersion(subgraphName: $name,) {
+    indexingStatusForCurrentVersion(subgraphName: $name) {
       synced
       health
       chains {
